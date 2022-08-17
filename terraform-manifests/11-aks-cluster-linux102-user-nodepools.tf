3849c574225b6bcadb7413b2b030e6aba7358a95
Sunil Chander <sun.chand.786@gmail.com>
2022-08-17 18:50:59 +0530

    node.tf added

--- terraform-manifests/11-aks-cluster-linux102-user-nodepools.tf
+++ terraform-manifests/11-aks-cluster-linux102-user-nodepools.tf
@@ -1,17 +1,17 @@
 # Create Linux Azure AKS Node Pool
-/*
+
 resource "azurerm_kubernetes_cluster_node_pool" "linux102" {
   availability_zones    = [1, 2, 3]
   enable_auto_scaling   = true
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks_cluster.id
-  max_count             = 3
+  max_count             = 2
   min_count             = 1
   mode                  = "User"
   name                  = "linux102"
   orchestrator_version  = data.azurerm_kubernetes_service_versions.current.latest_version
   os_disk_size_gb       = 30
   os_type               = "Linux" # Default is Linux, we can change to Windows
-  vm_size               = "Standard_DS2_v2"
+  vm_size               = "Standard_D2ads_v5"
   priority              = "Regular"  # Default is Regular, we can change to Spot with additional settings like eviction_policy, spot_max_price, node_labels and node_taints
   node_labels = {
     "nodepool-type" = "user"
@@ -26,4 +26,3 @@ resource "azurerm_kubernetes_cluster_node_pool" "linux102" {
     "ui-app"        = "reactjs-apps" 
   }
 }
-*/
